a28d8f7f792ddf911cc11f60d836abbac8ff4fe1
Vikas Kumar Saw <vikasvks0112@gmail.com>
2021-04-14 09:37:06 +0530

    Added Animations :))

---
+++ main.js
@@ -0,0 +1,16 @@
+gsap.from("header", {
+  opacity: 0,
+  duration: 1,
+  y: -50,
+});
+gsap.from("img", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.8,
+  y: 150,
+});
+gsap.from(".hero", {
+  opacity: 0,
+  duration: 1,
+  y: 200,
+});
